5e0219fb50507cb8e63ae373c4d3d237943a776e
Erica Snyder <ericaleesnyder@gmail.com>
2021-11-03 13:25:56 -0700

    added functionality to submit new trip button

---
+++ public/js/addtrip.js
@@ -0,0 +1,39 @@
+console.log("hello");
+
+
+const handleNewTripSubmit = async (event) => {
+  console.log("hello2");
+  event.preventDefault();
+  try {
+    const location = document.querySelector('#location').value.trim();
+    const startDate = document.querySelector('#startDate').value.trim();
+    const endDate = document.querySelector('#endDate').value.trim();
+    const rating = document.querySelector('#rating').value.trim();
+    const food = document.querySelector('#food').value.trim();
+    const experience = document.querySelector('#experience').value.trim();
+    const lesson = document.querySelector('#lesson').value.trim();
+    const revisit = document.querySelector('#revisit').value.trim();
+    
+    const tripResponse = await fetch('/api/trip', {
+      method: 'POST',
+      body: JSON.stringify({ location, startDate, endDate, rating, food, experience, lesson, revisit }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if (!tripResponse.ok) {
+      alert('Failed to create Trip.');
+      return;
+    }
+
+    // go to home page
+    // window.location.replace('/trip');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+document
+  .querySelector('.newTripSubmit')
+  .addEventListener('submit', handleNewTripSubmit);
